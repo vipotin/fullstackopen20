5c108e02c4b9f9ad3f4e47724127074f05372bb8
vipotin <vilma.pohjonen@gmail.com>
2020-03-06 14:11:32 +0200

    Unicafe 1.11 complete

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,9 +6,10 @@ const Button = (props) => {
     return <button onClick={props.handleClick}>{props.text}</button>
 }
 
-const StatisticLine = (props) => {
-    return <p>{props.text} {props.value}</p>
-}
+// Solution for 1.10
+// const StatisticLine = (props) => {
+//     return <p>{props.text} {props.value}</p>
+// }
 
 const Statistics = (props) => {
     const good = props.good
@@ -43,18 +44,46 @@ const Statistics = (props) => {
     return(
         <div>
             <h1>statistics</h1>
-            <StatisticLine text="good" value ={good} />
+            {/* Solution for 1.10 */}
+            {/* <StatisticLine text="good" value ={good} />
             <StatisticLine text="neutral" value ={neutral} />
             <StatisticLine text="bad" value ={bad} />
             <StatisticLine text="all" value={sumValues({good, neutral, bad})} />
             <StatisticLine text="average" value={averageValue({good, neutral, bad})} />
-            <StatisticLine text="positive" value={positivePercentage({good, neutral, bad})} unit="%" />
+            <StatisticLine text="positive" value={positivePercentage({good, neutral, bad})} unit="%" /> */}
+            <table>
+                <tbody>
+                    <tr>
+                        <td>good</td>
+                        <td>{good}</td>
+                    </tr>
+                    <tr>
+                        <td>neutral</td>
+                        <td>{neutral}</td>
+                    </tr>
+                    <tr>
+                        <td>bad</td>
+                        <td>{bad}</td>
+                    </tr>
+                    <tr>
+                        <td>all</td>
+                        <td>{sumValues({good, neutral, bad})}</td>
+                    </tr>
+                    <tr>
+                        <td>average</td>
+                        <td>{averageValue({good, neutral, bad})}</td>
+                    </tr>
+                    <tr>
+                        <td>positive</td>
+                        <td>{positivePercentage({good, neutral, bad})}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         )
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
