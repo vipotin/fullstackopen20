79b9d91131fb28b0ac5d84d817052425ba883240
vipotin <vilma.pohjonen@gmail.com>
2020-04-07 18:22:47 +0300

    5.7 complete

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,50 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const fullContent = (
+      <div>
+        {blog.title} {blog.author}
+        <br></br>{blog.url}
+        <br></br>likes {blog.likes} <button>like</button>
+      </div>
+  )
+
+  const shortContent = (
+    <div>
+      {blog.title} {blog.author}
+    </div>
+  )
+
+  const [showDetails, setShowDetails] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+  const [content, setContent] = useState(shortContent)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const changeView = () => {
+    setShowDetails(!showDetails)
+    if (showDetails) {
+      setButtonText('hide')
+      setContent(fullContent)
+    } else {
+      setButtonText('view')
+      setContent(shortContent)
+    }
+  }
+  
+  return (
+  <div style={blogStyle}>
+    <div>
+      {content}
+    </div>
+    <button onClick={changeView}>{buttonText}</button>
   </div>
-)
+  )
+}
 
 export default Blog
