b364ea280359e665d6e32643b69a0dcbeb0214b0
vipotin <vilma.pohjonen@gmail.com>
2020-04-15 14:07:16 +0300

    6.1 complete

---
+++ part6/src/reducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return { ...initialState, good: initialState.good + 1 }
+    case 'OK':
+      return { ...initialState, ok: initialState.ok + 1 }
+    case 'BAD':
+      return { ...initialState, bad: initialState.bad + 1 }
+    case 'ZERO':
+      return { good: 0, ok: 0, bad: 0}
+    default: return state
+  }
+  
+}
+
+export default counterReducer
