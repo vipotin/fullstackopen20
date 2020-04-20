24595118550aba16419545354aaaa4a5b737d3f4
vipotin <vilma.pohjonen@gmail.com>
2020-04-20 14:28:37 +0300

    7.2 complete

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Switch, Route, Link
+  Switch, Route, Link, useParams
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -17,11 +17,31 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({ anecdotes }) => {
+  console.log(anecdotes)
+  const id = useParams().id
+  console.log(id)
+  const anecdote = anecdotes.find(a => Number(a.id) === Number(id))
+  console.log(anecdote)
+  return (
+    <div>
+      <h2>{`${anecdote.content} by ${anecdote.author}`}</h2>
+      <p>{`has ${anecdote.votes} votes`}</p>
+      <p>
+        for more info see <a href={anecdote.info}>{anecdote.info}</a>
+      </p>
+    </div>
+  )
+}
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+      <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 )
@@ -132,6 +152,9 @@ const App = () => {
         <h1>Software anecdotes</h1>
         <Menu />
         <Switch>
+        <Route path='/anecdotes/:id'>
+            <Anecdote anecdotes={anecdotes} />
+          </Route>
           <Route path='/create'>
             <CreateNew addNew={addNew} />
           </Route>
