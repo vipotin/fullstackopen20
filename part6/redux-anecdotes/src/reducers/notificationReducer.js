ffa003aff6f2570895734d19db90c0c51eea2157
vipotin <vilma.pohjonen@gmail.com>
2020-04-20 10:36:31 +0300

    6.21 complete

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -3,12 +3,12 @@ const initialState = null
 export const setNotification = (message, time) => {
   return async dispatch => {
     await dispatch({ type: 'SET', data: message })
-    setTimeout(() => {
+    const timeoutID = setTimeout(() => {
       dispatch({ type: 'CLOSE' })
     }, (time*1000))
+    clearTimeout(timeoutID - 1)
   }
 }
-
 
 const reducer = (state = initialState, action) => {
 
