0c0c0ae0088dae898536cfce1a65c8ede22404df
vipotin <vilma.pohjonen@gmail.com>
2020-05-07 11:01:26 +0300

    8.10 complete

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+const NEW_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!){
+    addBook(
+      title: $title, 
+      author: $author, 
+      published: $published, 
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
+  const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [createBook] = useMutation(NEW_BOOK)
 
   if (!props.show) {
     return null
@@ -15,10 +34,12 @@ const NewBook = (props) => {
     event.preventDefault()
     
     console.log('add book...')
+    
+    createBook({ variables: { title, author, published: parseInt(published), genres } })
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
@@ -42,7 +63,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
