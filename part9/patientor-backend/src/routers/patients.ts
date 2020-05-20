133ee3fd403c363a30018d75b29a22310996065d
vipotin <vilma.pohjonen@gmail.com>
2020-05-20 10:34:10 +0300

    9.12 complete

--- part9/patientor-backend/src/routers/patients.ts
+++ part9/patientor-backend/src/routers/patients.ts
@@ -7,4 +7,16 @@ router.get('/', (_req, res) => {
   res.send(patientService.getPatientsWithoutSSN());
 });
 
+router.post('/', (req, res) => {
+  const { name, ssn, dateOfBirth, occupation, gender } = req.body;
+  const newPatient = {
+    name,
+    ssn,
+    dateOfBirth,
+    occupation,
+    gender
+  };
+  res.json(newPatient);
+});
+
 export default router;
