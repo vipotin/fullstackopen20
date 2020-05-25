0ad0aa134eb6eb94ca4dd6d1320818b0d9b87e24
vipotin <vilma.pohjonen@gmail.com>
2020-05-25 12:40:28 +0300

    9.22 complete

--- part9/patientor/src/PatientInfoPage/index.tsx
+++ part9/patientor/src/PatientInfoPage/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Container, Table, Button } from "semantic-ui-react";
+import { Container, Table, Button, CardContent } from "semantic-ui-react";
 import { useParams } from 'react-router-dom';
-import { Icon } from 'semantic-ui-react';
-import { Gender, Entry, Diagnosis } from '../types';
+import { Icon, Card } from 'semantic-ui-react';
+import { Gender, Entry, Diagnosis, HealthCheckRating } from '../types';
+import HealthRatingBar from '../components/HealthRatingBar';
 
 import { Patient } from "../types";
 import { apiBaseUrl } from "../constants";
 import { useStateValue, getPatient } from "../state";
+import { assertNever } from "../utils";
 
 const GenderIcon: React.FC<{ gender: Gender}> = ({ gender }) => {
   switch (gender) {
@@ -23,7 +25,7 @@ const GenderIcon: React.FC<{ gender: Gender}> = ({ gender }) => {
 };
 
 const PatientInfoPage: React.FC = () => {
-  const [{ selectedPatient, diagnosis }, dispatch] = useStateValue();
+  const [{ selectedPatient }, dispatch] = useStateValue();
   // const [error, setError] = React.useState<string | undefined>();
 
   const params = useParams<{ id: string}>();
@@ -48,12 +50,11 @@ const PatientInfoPage: React.FC = () => {
     // setError(undefined);
   }, []);
 
-  const findDiagnosis = (code: string): string | null => {
-    const foundDiagnosis: Diagnosis | undefined = diagnosis.find(d => d.code === code);
-    return foundDiagnosis ? foundDiagnosis.name : null;
-  };
+  // const findDiagnosis = (code: string): string | null => {
+  //   const foundDiagnosis: Diagnosis | undefined = diagnosis.find(d => d.code === code);
+  //   return foundDiagnosis ? foundDiagnosis.name : null;
+  // };
   
-
   if (!selectedPatient) return null;
 
   return (
@@ -65,17 +66,86 @@ const PatientInfoPage: React.FC = () => {
       </p>
       <h4>entries</h4>
       {selectedPatient.entries.map(entry => 
-        <div key={entry.id}>
-          <p>{entry.date} {entry.description}</p>
-          <ul>
-            {entry.diagnosisCodes ? entry.diagnosisCodes.map(code => 
-              <li key={code}>{code} {findDiagnosis(code)}</li>
-            ) : null }
-          </ul>
-        </div>
+        // <div key={entry.id}>
+        //   <p>{entry.date} {entry.description}</p>
+        //   <ul>
+        //     {entry.diagnosisCodes ? entry.diagnosisCodes.map(code => 
+        //       <li key={code}></li>
+        //     ) : null }
+        //   </ul>
+        // </div>
+        <EntryDetails key={entry.id} entry={entry} />
         )}
     </div>
   );
+};
+
+const HeartIcon: React.FC<{ rating: number }> = ({ rating }) => {
+  switch (rating) {
+    case 0:
+      return <Icon name="heart" color="green" />;
+    case 1:
+      return <Icon name="heart" color="yellow" />;
+    case 2:
+      return <Icon name="heart" color="orange" />;
+    case 3:
+      return <Icon name="heart" color="red" />;
+    default:
+      return null;
+  }
+};
+
+const HospitalEntry: React.FC<{ entry: Entry }> = ({ entry }) => {
+  return (
+    <Card fluid>
+      <CardContent>
+        <Card.Header>
+          {entry.date} <Icon name="hospital" size="large"/>
+        </Card.Header>
+        <Card.Description>{entry.description}</Card.Description>
+      </CardContent>
+    </Card>
+  );
+};
+
+const OccupationalHealthcare: React.FC<{ entry: Entry; employer: string }> = ({ entry, employer }) => {
+  return (
+    <Card fluid>
+      <CardContent>
+        <Card.Header>
+          {entry.date} <Icon name="stethoscope" size="large"/> {employer}
+        </Card.Header>
+        <Card.Description>{entry.description}</Card.Description>
+      </CardContent>
+    </Card>
+  );
+};
+
+const HealthCheck: React.FC<{ entry: Entry; rating: number }> = ({ entry, rating }) => {
+  return (
+    <Card fluid>
+      <CardContent>
+        <Card.Header>
+          {entry.date} <Icon name="doctor" size="large"/>
+        </Card.Header>
+        <Card.Description>{entry.description}</Card.Description>
+        <HeartIcon rating={rating} />
+      </CardContent>
+    </Card>
+  );
+};
+
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  switch (entry.type) {
+    case "Hospital":
+      return <HospitalEntry entry={entry} />;
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcare entry={entry} employer={entry.employerName} />;
+    case "HealthCheck":
+      return <HealthCheck entry={entry} rating={entry.healthCheckRating} />;
+    default:
+      return assertNever(entry);
+  }
 };
 
 export default PatientInfoPage;
